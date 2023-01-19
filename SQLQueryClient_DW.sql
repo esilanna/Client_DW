0f8d1a4aa2664bc97ffe3463d77d2e89afe21133
annalise <annalise@annalises-Mini.lan>
2023-01-19 12:14:07 -0500

    inital commit, SQL files added

---
+++ SQLQueryClient_DW.sql
@@ -0,0 +1,99 @@
+-- Creating Fact Schema for all of my fact tables --
+CREATE SCHEMA Fact;
+GO
+
+-- Creating Dimenion Schema for all of my dimension tables --
+CREATE SCHEMA Dimension;
+GO
+-- Verifying that both Schemas from above were successfully created --
+SELECT * FROM sys.schemas;
+
+-- Creating Date Dimension Table --
+CREATE TABLE Dimension.Date (
+    DateKey int NOT NULL,
+    FullDate date NOT NULL,
+    DayName nvarchar(9) NOT NULL,
+    DayNumber tinyint NOT NULL,
+    MonthName varchar(9) NOT NULL,
+    MonthNumber tinyint NOT NULL,
+    Year tinyint NOT NULL
+
+    CONSTRAINT PK_Date PRIMARY KEY CLUSTERED (
+        DateKey ASC
+    )
+);
+GO
+
+-- Creating Customer Dimension Table --
+CREATE TABLE Dimension.Customer (
+    CustomerKey int IDENTITY (1,1) NOT NULL,
+    CustomerAlternateKey int NOT NULL,
+    FirstName nvarchar(50) NOT NULL,
+    LastName nvarchar(50) NOT NULL,
+    FullName nvarchar(100) NOT NULL,
+    City nvarchar(50) NOT NULL,
+    State char(2) NOT NULL,
+    StateName nvarchar(20) NOT NULL,
+    Zip char(5) NOT NULL
+
+    CONSTRAINT PK_Customer PRIMARY KEY CLUSTERED (
+        CustomerKey ASC
+    )
+);
+GO
+
+-- Creating an Accounts Fact Table -- 
+CREATE TABLE Fact.Accounts (
+    AccountID int IDENTITY (1,1) NOT NULL,
+    DateKey int NOT NULL,
+    CustomerKey int NOT NULL,
+    InvoiceTotal money NOT NULL
+
+    CONSTRAINT PK_Account PRIMARY KEY CLUSTERED (
+        AccountID ASC
+    )
+);
+GO
+
+-- Adding the relationship between Accounts Fact Table and Date Dimension Table --
+ALTER TABLE Fact.Accounts WITH CHECK ADD CONSTRAINT FK_Fact_Accounts_DakeKey_Dimension_Date FOREIGN KEY(DateKey)
+REFERENCES Dimension.Date (DateKey);
+GO
+
+ALTER TABLE Fact.Accounts CHECK CONSTRAINT FK_Fact_Accounts_DakeKey_Dimension_Date;
+GO
+
+-- Adding the relationship between Accounts Fact Table and Customer Dimension Table --
+ALTER TABLE Fact.Accounts WITH CHECK ADD CONSTRAINT FK_Fact_Accounts_CustomerKey_Dimension_Customer FOREIGN KEY(CustomerKey)
+REFERENCES Dimension.Customer (CustomerKey);
+GO
+
+ALTER TABLE Fact.Accounts CHECK CONSTRAINT FK_Fact_Accounts_CustomerKey_Dimension_Customer;
+GO
+
+-- Creating a View --
+CREATE VIEW dbo.AccountsByDate
+    WITH SCHEMABINDING
+    AS
+        SELECT Accounts.AccountID, Accounts.CustomerKey, Accounts.InvoiceTotal,
+            Date.FullDate, Date.DayName, Date.DayNumber, Date.MonthName,
+            Date.MonthNumber, Date.Year
+        FROM Fact.Accounts JOIN Dimension.[Date] ON Accounts.DateKey = Date.DateKey
+;
+GO
+
+CREATE UNIQUE CLUSTERED INDEX IDX_AccountsByDate
+    ON dbo.AccountsByDate (AccountID);
+GO
+
+SELECT * FROM dbo.AccountsByDate;
+GO
+
+-- Creating a Columnstore Index for Performance Optimization of analytical queries --
+CREATE COLUMNSTORE INDEX IX_CS_FactAccounts
+ON Fact.Accounts (AccountID, DateKey, CustomerKey, InvoiceTotal);
+
+-- Rebuild Statement if fragmentation starts to happen (drops the index and rebuilds it entirely) --
+-- ALTER INDEXIX_CS_FactAccounts 
+-- ON Fact.Accounts
+-- REBUILD WITH (ONLINE = ON);
